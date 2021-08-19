6c8331d0d5851fa7d62ca353a5aacbc343a5fed9
tomonakar <tomohisa.nak@gmail.com>
2021-08-19 09:49:41 +0900

    test: Campaignのテストを追加

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -34,4 +34,61 @@ describe("Campaigns", () => {
     assert.ok(factory.options.address)
     assert.ok(campaign.options.address)
   })
+
+  it("marks caller as the campaign manager", async () => {
+    const manager = await campaign.methods.manager().call()
+    assert.equal(accounts[0], manager)
+
+    const isContributor = await campaign.methods.approvers(accounts[1])
+    assert(isContributor)
+  })
+
+  it("requires a minimum contribution", async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: "3",
+        from: accounts[1],
+      })
+      assert(false)
+    } catch (error) {
+      assert(error)
+    }
+  })
+
+  it("allows a manager to make a payment request", async () => {
+    await campaign.methods.createRequest("Buy Egg", "100", accounts[1]).send({
+      from: accounts[0],
+      gas: "1000000",
+    })
+
+    const request = await campaign.methods.requests(0).call()
+    assert.equal("Buy Egg", request.description)
+  })
+
+  it("process requests", async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei("10", "ether"),
+    })
+
+    await campaign.methods
+      .createRequest("A", web3.utils.toWei("5", "ether"), accounts[1])
+      .send({ from: accounts[0], gas: "1000000" })
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: "1000000",
+    })
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: "1000000",
+    })
+
+    let balance = await web3.eth.getBalance(accounts[1])
+    balance = web3.utils.fromWei(balance, "ether")
+    balance = parseFloat(balance)
+
+    assert(balance > 104)
+  })
 })
