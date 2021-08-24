61d8a450d13156f8a03f9b85a9779bc42979bcf7
tomonakar <tomohisa.nak@gmail.com>
2021-08-25 08:50:40 +0900

    chore: tailwindパージの設定を追加

--- kickstart-next/apps/campagin/tailwind.config.js
+++ kickstart-next/apps/campagin/tailwind.config.js
@@ -1,5 +1,12 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
 module.exports = {
+  purge: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
   presets: [require('../../tailwind-workspace-preset.js')],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
