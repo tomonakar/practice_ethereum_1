c5a4e11deb240afff127ea39ab34d6b54fd46a8a
tomonakar <tomohisa.nak@gmail.com>
2021-09-19 08:49:07 +0900

    feat: final

--- kickstart-next/apps/campagin/ethereum/deploy.js
+++ kickstart-next/apps/campagin/ethereum/deploy.js
@@ -1,26 +1,25 @@
-require("dotenv").config()
-const HDWalletProvider = require("truffle-hdwallet-provider")
-const Web3 = require("web3")
-const compiledFactory = require("./build/CampaignFactory.json")
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
   process.env.MNEMONIC,
   process.env.NETWORK,
-  1,
-)
-const web3 = new Web3(provider)
+  1
+);
+const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts()
+  const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", accounts[0])
+  console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(
-    JSON.parse(compiledFactory.interface),
+    JSON.parse(compiledFactory.interface)
   )
     .deploy({ data: compiledFactory.bytecode })
-    .send({ gas: "1000000", from: accounts[0] })
+    .send({ gas: '1000000', from: accounts[0] });
 
-  console.log("Contract deployed to", result.options.address)
-}
-deploy()
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
--- kickstart-next/apps/campagin/next.config.js
+++ kickstart-next/apps/campagin/next.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+require('dotenv');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -9,6 +10,13 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+    webpack5: false,
+    env: {
+      MNEMONIC: process.env.PRIVATE_KEY,
+      PRIVATE_KEY: process.env.PRIVATE_KEY,
+      NETWORK: process.env.NETWORK,
+      ADDRESS: process.env.ADDRESS,
+    },
   },
 };
 
